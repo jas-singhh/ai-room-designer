6b2d5ada281da311f5dc646a6522325f3f5ce579
Jaskaran Singh <jaswag99@gmail.com>
2025-07-25 18:08:00 +0100

    fix an environment variable issue

--- config/db.js
+++ config/db.js
@@ -1,2 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
-export const db = drizzle(process.env.DATABASE_URL);
+
+if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
+  throw new Error("NEXT_PUBLIC_DATABASE_URL environment variable is not set");
+}
+
+export const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_URL);
